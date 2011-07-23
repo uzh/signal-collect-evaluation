e081710524ec893c986b9858c9b1edde9de4a8d2
danistrebel <daniel.strebel@gmail.com>
2011-07-23 13:12:55 +0000

    ID in trait vertes is now def instead of val to allow custom serialization MemoryEfficientPage now has a custom serializer

--- src/main/scala/com/signalcollect/evaluation/util/MemoryEfficientPage.scala
+++ src/main/scala/com/signalcollect/evaluation/util/MemoryEfficientPage.scala
@@ -1,10 +1,31 @@
+/*
+ *  @author Philip Stutz
+ *  @author Daniel Strebel
+ *  
+ *  Copyright 2011 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
 package com.signalcollect.evaluation.util
 
 import com.signalcollect.interfaces._
 import com.signalcollect.implementations.graph.MostRecentSignalsMap
 import scala.collection.mutable.ArrayBuffer
+import java.io.{ObjectInput, ObjectOutput, Externalizable}
 
-class MemoryEfficientPage(val id: Int) extends Vertex[Int, Float] {
+class MemoryEfficientPage(var id: Int) extends Vertex[Int, Float] with Externalizable {
 
   var state = 0.15f
   var lastSignalState: Option[Float] = None
@@ -70,5 +91,36 @@ class MemoryEfficientPage(val id: Int) extends Vertex[Int, Float] {
   override def removeAllOutgoingEdges: Int = {
     throw new UnsupportedOperationException
   }
+  
+  def this() = this(-1) //default constructor for serialization
+ 
+ def writeExternal(out: ObjectOutput) {
+   out.writeInt(id)
+   out.writeFloat(state)
+   lastSignalState match {
+     case Some(oldState) => out.writeFloat(oldState)
+     case None => out.writeFloat(-1) //Safe because a page rank score should not be negative anyway
+   }
+   out.writeInt(targetIdArray.length)
+   for(i <- 0 until targetIdArray.length) {
+     out.writeInt(targetIdArray(i))
+   }
+ }
 
+ def readExternal(in: ObjectInput)  {
+   id = in.readInt // TODO set ID!!!!!
+   state = in.readFloat
+   val oldSignal = in.readFloat
+   oldSignal match {
+     case -1 => lastSignalState = None
+     case oldValue => lastSignalState = Some(oldValue)
+   }
+   val numberOfLinks = in.readInt
+   targetIdArray = new Array[Int](numberOfLinks)
+   for(i <- 0 until numberOfLinks) {
+     targetIdArray(i) = in.readInt
+   }
+ }
+
+ override def toString = "MemoryEfficientPage (" + id + ", " + state + ")"
 }
