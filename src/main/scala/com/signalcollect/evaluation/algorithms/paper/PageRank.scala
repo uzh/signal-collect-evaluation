0fce921ea05fcd0e05de85a601884683e321c4db
Philip Stutz <philip.stutz@gmail.com>
2013-10-14 00:08:40 +0200

    Modified paper exmple.

--- src/main/scala/com/signalcollect/evaluation/algorithms/paper/PageRank.scala
+++ src/main/scala/com/signalcollect/evaluation/algorithms/paper/PageRank.scala
@@ -2,19 +2,24 @@ package com.signalcollect.evaluation.algorithms.paper
 
 import com.signalcollect._
 
-class PageRankVertex(vertexId: Any, initialState: Double)
-    extends DataGraphVertex(vertexId, initialState) {
+class PageRankVertex(
+  id: Any, initialState: Double)
+  extends DataGraphVertex(id, initialState) {
   type Signal = Double
   def collect = 0.15 + 0.85 * signals.sum
 }
 
-class PageRankEdge(targetId: Any) extends DefaultEdge(targetId) {
+class PageRankEdge(targetId: Any)
+  extends DefaultEdge(targetId) {
   type Source = PageRankVertex
-  def signal = weight * source.state / source.sumOfOutWeights
+  def signal = source.state / source.edgeCount
 }
 
 object PageRankExample extends App {
-  val graph = GraphBuilder.build
+  val graph = GraphBuilder.
+    withStorageFactory(
+      factory.storage.JavaMapStorage).
+      build
   graph.addVertex(new PageRankVertex(1, 0.15))
   graph.addVertex(new PageRankVertex(2, 0.15))
   graph.addVertex(new PageRankVertex(3, 0.15))
@@ -22,7 +27,11 @@ object PageRankExample extends App {
   graph.addEdge(2, new PageRankEdge(1))
   graph.addEdge(2, new PageRankEdge(3))
   graph.addEdge(3, new PageRankEdge(2))
-  graph.execute
-  graph.foreachVertex(println(_))
+  graph.execute(
+    ExecutionConfiguration.
+      withSignalThreshold(0.001))
+  val top2 = graph.aggregate(
+    TopKFinder[Double](k = 2))
+  top2.foreach(println(_))
   graph.shutdown
 }
--- src/main/scala/com/signalcollect/evaluation/continuousperformance/SingleNodePageRank.scala
+++ src/main/scala/com/signalcollect/evaluation/continuousperformance/SingleNodePageRank.scala
@@ -1,139 +1,139 @@
-package com.signalcollect.evaluation.continuousperformance
-
-import com.signalcollect.nodeprovisioning.torque._
-import com.signalcollect.evaluation.resulthandling.GoogleDocsResultHandler
-import java.io._
-import scala.collection.mutable.ArrayBuffer
-import com.signalcollect._
-import com.signalcollect.factory.messagebus.BulkAkkaMessageBusFactory
-import com.signalcollect.configuration.ExecutionMode._
-import com.signalcollect.examples._
-import java.util.Date
-import java.text.SimpleDateFormat
-
-object SingleNodePageRank extends App {
-
-  val jvmParameters = " -Xmx28000m" +
-    " -Xms28000m"
-
-  val kraken = new TorqueHost(
-    jobSubmitter = new LocalJobSubmitter("strebel@ifi.uzh.ch"),
-    localJarPath = args(2), jvmParameters = jvmParameters, priority = TorquePriority.superfast)
-  val localHost = new LocalHost
-  val googleDocs = new GoogleDocsResultHandler(args(0), args(1), "continuous", "data")
-  val runsPerEvaluationRun = 10
-  val lastCommit = if (args.size >= 4) args(3) else "unknown"
-  
-
-  var evaluation = new Evaluation(evaluationName = "continous performance evaluation", executionHost = kraken).addResultHandler(googleDocs)
-
-  for (i <- 0 until runsPerEvaluationRun) {
-    evaluation = evaluation.addEvaluationRun(runPageRank)
-  }
-  
-  evaluation.addExtraStats(Map("commit" -> lastCommit))
-
-  evaluation.execute
-
-  def runPageRank(): List[Map[String, String]] = {
-		  
-    val lastCommit = if (args.size >= 4) args(3) else "unknown"
-      
-    def runEvaluation(): Map[String, String] = {
-
-      def cleanGarbage {
-        for (i <- 1 to 10) {
-          System.gc
-          Thread.sleep(100)
-        }
-        Thread.sleep(10000)
-      }
-
-      val g = new GraphBuilder[Int, Double].withMessageBusFactory(new BulkAkkaMessageBusFactory(1024, false)).build
-      val numberOfSplits = Runtime.getRuntime.availableProcessors
-      val splits = {
-        val s = new Array[DataInputStream](numberOfSplits)
-        for (i <- 0 until numberOfSplits) {
-          s(i) = new DataInputStream(new FileInputStream(System.getProperty("user.home") + s"/web-split-$i"))
-        }
-        s
-      }
-      def loadSplit(splitIndex: Int)(ge: GraphEditor[Int, Double]) {
-        val in = splits(splitIndex)
-        var vertexId = CompactIntSet.readUnsignedVarInt(in)
-        while (vertexId >= 0) {
-          val numberOfEdges = CompactIntSet.readUnsignedVarInt(in)
-          var edges = new ArrayBuffer[Int]
-          while (edges.length < numberOfEdges) {
-            val nextEdge = CompactIntSet.readUnsignedVarInt(in)
-            edges += nextEdge
-          }
-          val vertex = new EfficientPageRankVertex(vertexId)
-          vertex.setTargetIds(edges.length, CompactIntSet.create(edges.toArray))
-          ge.addVertex(vertex)
-          vertexId = CompactIntSet.readUnsignedVarInt(in)
-        }
-      }
-
-      for (i <- 0 until numberOfSplits) {
-        g.modifyGraph(loadSplit(i), Some(i))
-      }
-      print("Loading graph ...")
-      g.awaitIdle
-      println("done.")
-
-      //make sure that all garbage is collected before starting the computation
-      cleanGarbage
-
-      print("Running computation ...")
-      val stats = g.execute(ExecutionConfiguration.withExecutionMode(PureAsynchronous).withSignalThreshold(0.001))
-      println("done.")
-
-      val startDate = new Date
-      val dateFormat = new SimpleDateFormat("dd-MM-yyyy")
-      val timeFormat = new SimpleDateFormat("HH:mm:ss")
-
-      var statsMap = Map[String, String]()
-      statsMap += (("startDate", dateFormat.format(startDate)))
-      statsMap += (("startTime", timeFormat.format(startDate)))
-
-      if (stats != null) {
-        statsMap += (("numberOfWorkers", stats.numberOfWorkers.toString))
-        statsMap += (("computationTimeInMilliseconds", stats.executionStatistics.computationTime.toMillis.toString))
-        statsMap += (("jvmCpuTimeInMilliseconds", stats.executionStatistics.jvmCpuTime.toMillis.toString))
-        statsMap += (("totalExecutionTimeInMilliseconds", stats.executionStatistics.totalExecutionTime.toMillis.toString))
-        statsMap += (("terminationReason", stats.executionStatistics.terminationReason.toString))
-        statsMap += (("executionMode", stats.parameters.executionMode.toString))
-        statsMap += (("workerFactory", stats.config.workerFactory.toString))
-        statsMap += (("storageFactory", stats.config.storageFactory.toString))
-        statsMap += (("messageBusFactory", stats.config.messageBusFactory.toString))
-        statsMap += (("logger", stats.config.logger.toString))
-        statsMap += (("signalSteps", stats.executionStatistics.signalSteps.toString))
-        statsMap += (("collectSteps", stats.executionStatistics.collectSteps.toString))
-        statsMap += (("numberOfVertices", stats.aggregatedWorkerStatistics.numberOfVertices.toString))
-        statsMap += (("numberOfEdges", stats.aggregatedWorkerStatistics.numberOfOutgoingEdges.toString))
-        statsMap += (("collectOperationsExecuted", stats.aggregatedWorkerStatistics.collectOperationsExecuted.toString))
-        statsMap += (("signalOperationsExecuted", stats.aggregatedWorkerStatistics.signalOperationsExecuted.toString))
-        statsMap += (("stepsLimit", stats.parameters.stepsLimit.toString))
-        statsMap += (("signalThreshold", stats.parameters.signalThreshold.toString.replace('.', ',')))
-        statsMap += (("collectThreshold", stats.parameters.collectThreshold.toString.replace('.', ',')))
-      } else {
-        println(stats)
-      }
-      g.shutdown
-      statsMap
-    }
-
-    var result = List[Map[String, String]]()
-    
-    //run warm up
-    for(_ <- 0 until 20) {
-      runEvaluation()
-    }
-    
-    //measure
-    result = runEvaluation() :: result
-
-    result
-  }
-}
+//package com.signalcollect.evaluation.continuousperformance
+//
+//import com.signalcollect.nodeprovisioning.torque._
+//import com.signalcollect.evaluation.resulthandling.GoogleDocsResultHandler
+//import java.io._
+//import scala.collection.mutable.ArrayBuffer
+//import com.signalcollect._
+//import com.signalcollect.factory.messagebus.BulkAkkaMessageBusFactory
+//import com.signalcollect.configuration.ExecutionMode._
+//import com.signalcollect.examples._
+//import java.util.Date
+//import java.text.SimpleDateFormat
+//
+//object SingleNodePageRank extends App {
+//
+//  val jvmParameters = " -Xmx28000m" +
+//    " -Xms28000m"
+//
+//  val kraken = new TorqueHost(
+//    jobSubmitter = new LocalJobSubmitter("strebel@ifi.uzh.ch"),
+//    localJarPath = args(2), jvmParameters = jvmParameters, priority = TorquePriority.superfast)
+//  val localHost = new LocalHost
+//  val googleDocs = new GoogleDocsResultHandler(args(0), args(1), "continuous", "data")
+//  val runsPerEvaluationRun = 10
+//  val lastCommit = if (args.size >= 4) args(3) else "unknown"
+//  
+//
+//  var evaluation = new Evaluation(evaluationName = "continous performance evaluation", executionHost = kraken).addResultHandler(googleDocs)
+//
+//  for (i <- 0 until runsPerEvaluationRun) {
+//    evaluation = evaluation.addEvaluationRun(runPageRank)
+//  }
+//  
+//  evaluation.addExtraStats(Map("commit" -> lastCommit))
+//
+//  evaluation.execute
+//
+//  def runPageRank(): List[Map[String, String]] = {
+//		  
+//    val lastCommit = if (args.size >= 4) args(3) else "unknown"
+//      
+//    def runEvaluation(): Map[String, String] = {
+//
+//      def cleanGarbage {
+//        for (i <- 1 to 10) {
+//          System.gc
+//          Thread.sleep(100)
+//        }
+//        Thread.sleep(10000)
+//      }
+//
+//      val g = new GraphBuilder[Int, Double].withMessageBusFactory(new BulkAkkaMessageBusFactory(1024, false)).build
+//      val numberOfSplits = Runtime.getRuntime.availableProcessors
+//      val splits = {
+//        val s = new Array[DataInputStream](numberOfSplits)
+//        for (i <- 0 until numberOfSplits) {
+//          s(i) = new DataInputStream(new FileInputStream(System.getProperty("user.home") + s"/web-split-$i"))
+//        }
+//        s
+//      }
+//      def loadSplit(splitIndex: Int)(ge: GraphEditor[Int, Double]) {
+//        val in = splits(splitIndex)
+//        var vertexId = CompactIntSet.readUnsignedVarInt(in)
+//        while (vertexId >= 0) {
+//          val numberOfEdges = CompactIntSet.readUnsignedVarInt(in)
+//          var edges = new ArrayBuffer[Int]
+//          while (edges.length < numberOfEdges) {
+//            val nextEdge = CompactIntSet.readUnsignedVarInt(in)
+//            edges += nextEdge
+//          }
+//          val vertex = new EfficientPageRankVertex(vertexId)
+//          vertex.setTargetIds(edges.length, CompactIntSet.create(edges.toArray))
+//          ge.addVertex(vertex)
+//          vertexId = CompactIntSet.readUnsignedVarInt(in)
+//        }
+//      }
+//
+//      for (i <- 0 until numberOfSplits) {
+//        g.modifyGraph(loadSplit(i), Some(i))
+//      }
+//      print("Loading graph ...")
+//      g.awaitIdle
+//      println("done.")
+//
+//      //make sure that all garbage is collected before starting the computation
+//      cleanGarbage
+//
+//      print("Running computation ...")
+//      val stats = g.execute(ExecutionConfiguration.withExecutionMode(PureAsynchronous).withSignalThreshold(0.001))
+//      println("done.")
+//
+//      val startDate = new Date
+//      val dateFormat = new SimpleDateFormat("dd-MM-yyyy")
+//      val timeFormat = new SimpleDateFormat("HH:mm:ss")
+//
+//      var statsMap = Map[String, String]()
+//      statsMap += (("startDate", dateFormat.format(startDate)))
+//      statsMap += (("startTime", timeFormat.format(startDate)))
+//
+//      if (stats != null) {
+//        statsMap += (("numberOfWorkers", stats.numberOfWorkers.toString))
+//        statsMap += (("computationTimeInMilliseconds", stats.executionStatistics.computationTime.toMillis.toString))
+//        statsMap += (("jvmCpuTimeInMilliseconds", stats.executionStatistics.jvmCpuTime.toMillis.toString))
+//        statsMap += (("totalExecutionTimeInMilliseconds", stats.executionStatistics.totalExecutionTime.toMillis.toString))
+//        statsMap += (("terminationReason", stats.executionStatistics.terminationReason.toString))
+//        statsMap += (("executionMode", stats.parameters.executionMode.toString))
+//        statsMap += (("workerFactory", stats.config.workerFactory.toString))
+//        statsMap += (("storageFactory", stats.config.storageFactory.toString))
+//        statsMap += (("messageBusFactory", stats.config.messageBusFactory.toString))
+//        statsMap += (("logger", stats.config.logger.toString))
+//        statsMap += (("signalSteps", stats.executionStatistics.signalSteps.toString))
+//        statsMap += (("collectSteps", stats.executionStatistics.collectSteps.toString))
+//        statsMap += (("numberOfVertices", stats.aggregatedWorkerStatistics.numberOfVertices.toString))
+//        statsMap += (("numberOfEdges", stats.aggregatedWorkerStatistics.numberOfOutgoingEdges.toString))
+//        statsMap += (("collectOperationsExecuted", stats.aggregatedWorkerStatistics.collectOperationsExecuted.toString))
+//        statsMap += (("signalOperationsExecuted", stats.aggregatedWorkerStatistics.signalOperationsExecuted.toString))
+//        statsMap += (("stepsLimit", stats.parameters.stepsLimit.toString))
+//        statsMap += (("signalThreshold", stats.parameters.signalThreshold.toString.replace('.', ',')))
+//        statsMap += (("collectThreshold", stats.parameters.collectThreshold.toString.replace('.', ',')))
+//      } else {
+//        println(stats)
+//      }
+//      g.shutdown
+//      statsMap
+//    }
+//
+//    var result = List[Map[String, String]]()
+//    
+//    //run warm up
+//    for(_ <- 0 until 20) {
+//      runEvaluation()
+//    }
+//    
+//    //measure
+//    result = runEvaluation() :: result
+//
+//    result
+//  }
+//}
